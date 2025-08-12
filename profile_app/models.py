a10ac111eed575d0af5bc0b2af62ff5f1a58be12
HPetersen2 <petermann2@web.de>
2025-08-12 10:30:24 +0200

    create model for profile

--- profile_app/models.py
+++ profile_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    file = models.ImageField(upload_to='uploads/')
+    location = models.CharField(max_length=50)
+    tel = models.IntegerField()
+    description = models.CharField(max_length=255)
+    working_hours = models.CharField(max_length=20)
