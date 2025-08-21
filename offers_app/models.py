e866898c7ed19a680a2b33ab66bf5f8db49b5041
HPetersen2 <petermann2@web.de>
2025-08-21 15:36:23 +0200

    add offer and offerdetail model

--- offers_app/models.py
+++ offers_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Offer(models.Model):
+    title = models.CharField(max_length=50)
+    image = models.FileField(upload_to="media/", blank=True, null=True)
+    description = models.CharField(max_length=255)
+
+class OfferDetail(models.Model):
+    title = models.CharField(max_length=50)
+    revisions = models.IntegerField()
