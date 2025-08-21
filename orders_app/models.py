a21f203a74857a33e3a2a0daae46d1d91cf950d4
HPetersen2 <petermann2@web.de>
2025-08-21 15:36:32 +0200

    change order model

--- orders_app/models.py
+++ orders_app/models.py
@@ -1,16 +1,26 @@
 from django.db import models
-from auth_app.models import UserProfile
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 STATUS_CHOICES = [
     ("in_progress", "In Progress"),
     ("completed", "Completed"),
 ]
 
+OFFER_CHOICES = [
+    ("basic", "basic")
+]
+
 class Order(models.Model):
-    customer_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="customer_oders")
-    business_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="business_orders")
+    customer_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="customer_oders")
+    business_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="business_orders")
     title = models.CharField(max_length=50)
-    revisions = models.IntegerField()
+    revisions = models.PositiveIntegerField(default=0)
+    delivery_time_in_days = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    features = models.JSONField(default=list)
+    offer_type = models.CharField(max_length=25, choices=OFFER_CHOICES)
     status = models.CharField(max_length=25, choices=STATUS_CHOICES, default="in-progress")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
