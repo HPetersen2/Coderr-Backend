3d4c423493cc84252ce9070dc879fbf7fb6b38f0
HPetersen2 <petermann2@web.de>
2025-08-21 07:52:12 +0200

    add order model

--- orders_app/models.py
+++ orders_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from auth_app.models import UserProfile
 
-# Create your models here.
+STATUS_CHOICES = [
+    ("in_progress", "In Progress"),
+    ("completed", "Completed"),
+]
+
+class Order(models.Model):
+    customer_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="customer_oders")
+    business_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="business_orders")
+    title = models.CharField(max_length=50)
+    revisions = models.IntegerField()
+    status = models.CharField(max_length=25, choices=STATUS_CHOICES, default="in-progress")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
