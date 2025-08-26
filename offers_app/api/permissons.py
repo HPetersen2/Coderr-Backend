9c113ffbc994ebdf4c7eef85b2d1e67a372a0cfb
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2025-08-26 08:36:50 +0200

    add permission for business user

---
+++ offers_app/api/permissons.py
@@ -0,0 +1,10 @@
+from rest_framework.permissions import BasePermission
+
+class IsBusinessUser(BasePermission):
+    def has_permission(self, request, view):
+        user_profile = getattr(request.user, "userprofile", None)
+        return bool(
+            request.user.is_authenticated
+            and user_profile is not None
+            and user_profile.type == "business"
+        )
