e16de51635364b46a0d27522ccac131103ba512a
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2025-08-26 08:37:18 +0200

    add validate serializer for post offer

--- offers_app/api/serializers.py
+++ offers_app/api/serializers.py
@@ -1,8 +1,28 @@
 from rest_framework import serializers
-from offers_app.models import Offer
+from offers_app.models import Offer, OfferDetail
+
+class OfferDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OfferDetail
+        fields = ['id', 'title', 'revisions', 'delivery_time_in_days', 'price', 'features', 'offer_type']
+        extra_kwargs = {
+            "offer": {"read_only": True}
+        }
 
 class OfferSerizalizer(serializers.ModelSerializer):
-    details = serializers.JSONField(source='detail')
+    details = OfferDetailSerializer(many=True)
     class Meta:
         model = Offer
-        fields = '__all__'
+        fields = ['id', 'title', 'image', 'description', 'details']
+
+    def validate_details(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError("Ein Offer muss mindestens 3 Details enthalten.")
+        return value
+
+    def create(self, validated_data):
+        details_data = validated_data.pop("details")
+        offer = Offer.objects.create(**validated_data)
+        for detail in details_data:
+            OfferDetail.objects.create(offer=offer, **detail)
+        return offer
--- offers_app/api/views.py
+++ offers_app/api/views.py
@@ -1,9 +1,19 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from .permissons import IsBusinessUser
 from offers_app.models import Offer
 from offers_app.api.serializers import OfferSerizalizer
 
 class OfferListView(generics.ListCreateAPIView):
     queryset = Offer.objects.all()
     serializer_class = OfferSerizalizer
-    permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            permission_classes = [IsAuthenticated]
+        elif self.request.method == 'POST':
+            permission_classes = [IsBusinessUser]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
