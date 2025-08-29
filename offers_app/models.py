f94b1235a14b576f9fac737d0dbccddd0d481d40
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2025-08-29 15:39:14 +0200

    add created_at and updated_at on offer model

--- offers_app/models.py
+++ offers_app/models.py
@@ -10,10 +10,12 @@ OFFER_CHOICES = [
 ]
 
 class Offer(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='offers')
     title = models.CharField(max_length=50)
     image = models.FileField(upload_to="media/", blank=True, null=True)
     description = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 class OfferDetail(models.Model):
     offer = models.ForeignKey(Offer, related_name="details", on_delete=models.CASCADE)
