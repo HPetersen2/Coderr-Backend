c041bc5b048ac0313832557d258acebffc198ea1
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2025-08-31 18:28:52 +0200

    add get queryset when customer user

--- orders_app/api/views.py
+++ orders_app/api/views.py
@@ -1,9 +1,16 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
 from orders_app.models import Order
 from .serializers import OrderListSerializer
 
 class OrderListView(generics.ListAPIView):
-    queryset = Order.objects.all()
     serializer_class = OrderListSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        if self.request.user.userprofile.type == 'business':
+            raise PermissionDenied(detail='Business users cannot view orders.')
+        else:
+            return Order.objects.filter(customer_user=self.request.user)
+    
