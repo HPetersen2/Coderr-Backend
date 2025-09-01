f47539c6b24b4415cde6da122dfafd9427f22897
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2025-09-01 16:21:31 +0200

    add offers

--- offers_app/api/serializers.py
+++ offers_app/api/serializers.py
@@ -34,30 +34,54 @@ class OfferDetailSerializer(serializers.ModelSerializer):
         model = OfferDetail
         fields = ['id', 'title', 'revisions', 'delivery_time_in_days', 'price', 'features', 'offer_type']
         extra_kwargs = {
-            "offer": {"read_only": True},
-            "user": {"read_only": True}
+            'offer': {'read_only': True},
+            'user': {'read_only': True}
         }
 
 class OfferPostSerializer(serializers.ModelSerializer):
     details = OfferDetailSerializer(many=True)
+
     class Meta:
         model = Offer
         fields = ['id', 'title', 'image', 'description', 'details']
 
     def validate_details(self, value):
         if len(value) < 3:
-            raise serializers.ValidationError("Ein Offer muss mindestens 3 Details enthalten.")
+            raise serializers.ValidationError('Ein Offer muss mindestens 3 Details enthalten.')
         return value
 
     def create(self, validated_data):
-        details_data = validated_data.pop("details")
+        details_data = validated_data.pop('details')
         offer = Offer.objects.create(**validated_data)
         for detail in details_data:
             OfferDetail.objects.create(offer=offer, **detail)
         return offer
     
+    def update(self, instance, validated_data):
+        details_data = validated_data.pop('details', None)
+
+        instance = super().update(instance, validated_data)
+
+        if details_data is not None:
+            if len(details_data) < 3:
+                raise serializers.ValidationError('Ein Offer muss mindestens 3 Details enthalten.')
+
+            for detail_data in details_data:
+                detail_id = detail_data.get('id')
+
+                if detail_id:
+                    try:
+                        offer_detail = instance.details.get(id=detail_id)
+                        for attr, value in detail_data.items():
+                            setattr(offer_detail, attr, value)
+                        offer_detail.save()
+                    except OfferDetail.DoesNotExist:
+                        raise serializers.ValidationError(f"OfferDetail mit ID {detail_id} existiert nicht.")
+                else:
+                    OfferDetail.objects.create(offer=instance, **detail_data)
+    
 class OfferSerializer(serializers.ModelSerializer):
-    details = OfferDetailGetSerializer(many=True)
+    details = OfferDetailGetSerializer(many=True, read_only=True)
     min_price = serializers.DecimalField(max_digits=10, decimal_places=2, read_only=True)
     min_delivery_time = serializers.IntegerField(read_only=True)
     class Meta:
