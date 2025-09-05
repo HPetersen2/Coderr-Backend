33915fee43fc3e86076abee8035092e0b2496fd6
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2025-09-05 15:09:15 +0200

    add script for backup

---
+++ backup.sh
@@ -0,0 +1,18 @@
+echo "Database Backup started"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+BACKUP_NAME="db-backup__`date "+%F__%H:%M"`.db"
+cp $SCRIPT_DIR/../db.sqlite3 $BACKUP_NAME
+echo "Uploading file:  $BACKUP_NAME"
+
+echo "
+ verbose
+ open backup.henrik-petersen.de
+ user backup-admin O172?3zeu
+ bin
+ put $BACKUP_NAME
+ bye
+" | ftp -n > ftp_$$.log
+echo "Backup successful"
+
+rm $BACKUP_NAME
+rm ftp_$$.log
